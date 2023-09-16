85808090d24401e829600b5a5f1f61f748ec139f
mohamim360 <mohamim360@gmail.com>
2023-09-16 09:03:02 +0600

    Cors

--- index.js
+++ index.js
@@ -12,9 +12,17 @@ const chatRoutes = require("./routes/chat");
 
 const app = express();
 
-app.use(bodyParser.json());  //application/json
+app.use(bodyParser.json()); //application/json
 
 app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization");
+// 	next()
+// });
+ //This middleware is responsible for setting up Cross-Origin Resource Sharing (CORS) headers to allow cross-origin requests to your server
 
 app.use("/chat", chatRoutes);
 
