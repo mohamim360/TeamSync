7ee25f91a3842c2e0b0a9509da70255ece29699c
mohamim360 <mohamim360@gmail.com>
2024-12-12 20:04:04 +0600

    get

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(cors());
 
 app.use("/chat", chatRoutes);
 app.use("/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to Team Sync Server!");
+});
+
 
 mongoose.connect(process.env.DB_URL).then((result) => {
   const server = app.listen(3000);
